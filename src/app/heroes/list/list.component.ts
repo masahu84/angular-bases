719df4ee829a0a788dbed90982ee96c92b8b696d
ATSISTEMAS\msanchez.humanes <msanchez.humanes@atsistemas.com>
2024-01-25 10:19:34 +0100

    Fin de sección 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heores-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  public heroNames: string[] = [
+    'Spiderman',
+    'Ironman',
+    'Hulk',
+    'She Hulk',
+    'Thor',
+  ];
+
+  public deletedHero?: string;
+
+  removeLastHero(): void {
+    this.deletedHero = this.heroNames.pop();
+  }
+}
